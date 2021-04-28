6c5d016b8854c263ddbe5be0815e9e56b04e3bd5
Divya Nayal <43030745+divyanayal@users.noreply.github.com>
2021-04-28 20:20:39 +0530

    Initial commit

---
+++ selection/forms.py
@@ -0,0 +1,80 @@
+from django.contrib.auth.forms import UserCreationForm
+from .models import *
+from django import forms
+from django.core.exceptions import ValidationError
+import datetime
+
+YEARS= [x for x in range(2018,2020)]
+
+class UserForm(UserCreationForm):
+    password1 = forms.CharField(min_length=8, max_length=30, widget=forms.PasswordInput(render_value=False))
+
+    class Meta:
+        model = User
+        fields = ['username', 'password1', 'password2']
+        help_texts = {
+            'username': 'same as your smart card id',
+        }
+
+    # def clean_password(self):
+    #     password = self.cleaned_data.get('password1')
+    #     if len(password) < 8:
+    #         raise ValidationError('Password too short')
+    #     return super(UserCreationForm, self).clean_password1()
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+
+
+class RegistrationForm(forms.ModelForm):
+
+    class Meta:
+        model = Student
+        fields = [
+            'student_name',
+            'father_name',
+            'smart_card_id',
+            'course',
+            'year_of_study',
+            'dob']
+
+
+class SelectionForm(forms.ModelForm):
+    class Meta:
+        model = Student
+        fields = ['room']
+
+
+class DuesForm(forms.Form):
+    choice = forms.ModelChoiceField(queryset=Student.objects.all().filter(no_dues=True))
+
+
+class NoDuesForm(forms.Form):
+    choice = forms.ModelChoiceField(queryset=Student.objects.all().filter(no_dues=False))
+
+
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
+
+class LeaveForm(forms.ModelForm):
+    start_date = forms.DateField(initial=datetime.date.today, widget=forms.SelectDateWidget(years=YEARS))
+    end_date = forms.DateField(initial=datetime.date.today, widget=forms.SelectDateWidget(years=YEARS))
+    reason = forms.CharField(max_length=100, help_text='100 characters max.',
+                             widget=forms.TextInput(attrs={'placeholder': 'Enter Reason here'}))
+    class Meta:
+        model = Leave
+        fields = [
+            'start_date',
+            'end_date',
+            'reason']
+class RepairForm(forms.ModelForm):
+    class Meta:
+        model = Room
+        fields = ['repair']
+
+
+class RebateForm(forms.Form):
+    rebate = forms.DateField(initial=datetime.date.today, widget=forms.SelectDateWidget(years=YEARS))
---
+++ selection/models.py
@@ -0,0 +1,135 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+import datetime
+
+class User(AbstractUser):
+    is_warden = models.BooleanField(default=False)
+
+
+class Student(models.Model):
+    user = models.OneToOneField(
+        User,
+        default=None,
+        null=True,
+        on_delete=models.CASCADE)
+    year_choices = [('I','First'),('II', 'Second'), ('III', 'Third'), ('IV', 'Fourth')]
+    student_name = models.CharField(max_length=200, null=True)
+    father_name = models.CharField(max_length=200, null=True)
+    smart_card_id = models.CharField(max_length=20, unique=True, null=True)
+    course = models.ForeignKey(
+        'Course',
+        null=True,
+        default=None,
+        on_delete=models.CASCADE)
+    dob = models.DateField(
+        max_length=10,
+        help_text="format : YYYY-MM-DD",
+        null=True)
+    year_of_study = models.CharField(
+        choices=year_choices,
+        max_length=3,
+        default='I',null=True)
+    room = models.OneToOneField(
+        'Room',
+        blank=True,
+        on_delete= models.SET_NULL,
+        null=True)
+    room_allotted = models.BooleanField(default=False)
+    no_dues = models.BooleanField(default=True)
+
+    def __str__(self):
+        return str(self.smart_card_id)
+
+    def delete(self, *args, **kwargs):
+        room_del = Room.objects.filter(student__room=self.room)
+        print('pppppppppppppppppppppppppppppppppppppppp')
+        for s in room_del:
+            s.vacant = True
+            s.save()
+            print('***********')
+        super(Student, self).delete(*args, **kwargs)
+
+
+class Room(models.Model):
+    room_choice = [('S', 'Single Occupancy'), ('D', 'Double Occupancy'), ('T', 'Triple vacancy'),('A', 'All Single, Double and Triple Occupancy')]
+    no = models.CharField(max_length=5)
+    name = models.CharField(max_length=10)
+    room_type = models.CharField(choices=room_choice, max_length=1, default=None)
+    vacant = models.BooleanField(default=False)
+    hostel = models.ForeignKey('Hostel', on_delete=models.CASCADE)
+    repair = models.CharField(max_length=100, blank=True)
+
+
+    def __str__(self):
+        return '%s %s' %(self.name, self.hostel)
+
+    def delete(self, *args, **kwargs):
+        stud = Student.objects.filter(room=self)
+        print('pppppppppppppppppppppppppppppppppppppppp')
+        for s in stud:
+            s.room_allotted = False
+            s.save()
+            print('***********')
+        super(Room, self).delete(*args, **kwargs)
+
+
+class Hostel(models.Model):
+    name = models.CharField(max_length=50)
+    year_choices = [('I','First'),('II', 'Second'), ('III', 'Third'), ('IV', 'Fourth')]
+    year_of_study = models.CharField(
+        choices=year_choices,
+        max_length=3,
+        default=None,null=True)
+    course = models.ManyToManyField('Course', default=None, blank=True)
+    caretaker = models.CharField(max_length=100, blank=True)
+
+
+    def __str__(self):
+        return self.name
+
+
+class Course(models.Model):
+    # if a student has enrollment number iit2017001 then the course code is iit2017
+    code = models.CharField(max_length=100, default=None)
+    room_choice = [('S', 'Single Occupancy'), ('D', 'Double Occupancy'), ('T', 'Triple vacancy'),('A', 'All Single, Double and Triple Occupancy')]
+    room_type = models.CharField(choices=room_choice, max_length=1, default='A')
+
+    def __str__(self):
+        return self.code
+
+
+class Warden(models.Model):
+    user = models.OneToOneField(
+        User,
+        default=None,
+        null=True,
+        on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, null=True)
+    hostel = models.ForeignKey('Hostel',default=None,null=True,
+                               on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if self.user.is_warden is False:  # Set default reference
+            self.user.is_warden = True
+            self.user.save()
+        super(Warden, self).save(*args, **kwargs)
+
+    def delete(self, *args, **kwargs):
+        self.user.is_warden = False
+        self.user.save()
+        print('pppppppppppppppppppppppppppppppppppppppp')
+
+        super(Warden, self).delete(*args, **kwargs)
+
+
+class Leave(models.Model):
+    student = models.ForeignKey('Student', on_delete=models.CASCADE)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    reason = models.CharField(max_length=100,blank = False)
+    accept = models.BooleanField(default=False)
+    reject = models.BooleanField(default=False)
+    confirm_time = models.DateTimeField(auto_now_add=True)
